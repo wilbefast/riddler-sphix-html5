fca394811d2b36f7e863e7a1e35dd07c58cd3ac4
wilbefast <wilbefast@gmail.com>
2014-11-08 08:44:48 +0100

    using SSL so that permissions will be kept between plays

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 var express = require('express');
+var https = require('https');
+var fs = require('fs');
+var express = require('express');
+
+var privateKey  = fs.readFileSync('sslcert/server.key', 'utf8');
+var certificate = fs.readFileSync('sslcert/server.crt', 'utf8');
 
 var app = express();
-
 app.use("/", express.static(__dirname));
-app.listen(3000);
+
+var httpsServer = https.createServer({
+	key: privateKey, 
+	cert: certificate
+}, app);
+
+httpsServer.listen(3000);
